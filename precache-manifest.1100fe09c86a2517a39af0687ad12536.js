c3059cf29af7bc9462d6fa80a30982afcb57d79e
Katrusy86 <katrin.myronova@gmail.com>
2020-04-29 11:38:55 +0300

    Updates

---
+++ precache-manifest.1100fe09c86a2517a39af0687ad12536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa97f91ee6a26821c3dbb00d80bfd6f6",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "87614f9abe7ad3c4768d",
+    "url": "/goit-react-hw-01-components/static/css/main.3afdb392.chunk.css"
+  },
+  {
+    "revision": "83677935d6b711ad98bd",
+    "url": "/goit-react-hw-01-components/static/js/2.82865cd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.82865cd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87614f9abe7ad3c4768d",
+    "url": "/goit-react-hw-01-components/static/js/main.c213f907.chunk.js"
+  },
+  {
+    "revision": "5a18dd9396dd6a18bba5",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.702437d4.js"
+  }
+]);
